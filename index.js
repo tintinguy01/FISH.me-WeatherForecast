9228625fdbbf575aac0e072c9c2e780061c17fd1
Swiss Tangsatjatham <tintinguy.01@gmail.com>
2024-08-26 22:54:46 +0700

    updated index.js

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ import bodyParser from 'body-parser';
 import axios from 'axios';
 import path from 'path';
 import dotenv from 'dotenv';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 
 // Load environment variables from .env file
 dotenv.config();
